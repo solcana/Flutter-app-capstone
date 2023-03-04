9d9807feb4d3a67747160b9138a838706731f290
Ana Solcan <solcan.anna@gmail.com>
2023-03-04 16:25:39 +0200

    created new Destination model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_140202) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_142443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "destinations", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "travel_logs", force: :cascade do |t|
     t.string "category"
