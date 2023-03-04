e4db48c1095c13380f52b7b4dd4c0cd1d83ad5f5
Ana Solcan <solcan.anna@gmail.com>
2023-03-04 16:39:55 +0200

    added association between travelLogs and destination

--- app/models/travel_log.rb
+++ app/models/travel_log.rb
@@ -1,3 +1,4 @@
 class TravelLog < ApplicationRecord
   belongs_to :user
+  belongs_to :destination
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_142443) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_142608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_142443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "destination_id", null: false
+    t.index ["destination_id"], name: "index_travel_logs_on_destination_id"
     t.index ["user_id"], name: "index_travel_logs_on_user_id"
   end
 
@@ -45,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_142443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "travel_logs", "destinations"
   add_foreign_key "travel_logs", "users"
 end
