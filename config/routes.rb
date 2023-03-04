a4ae5bd95654fbb1796977625a75964fbb263512
Ana Solcan <solcan.anna@gmail.com>
2023-03-04 15:56:20 +0200

    added routes for user and travel_log

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 devise_for :users
+
+resources :travel_logs
+resources :users
+
   get "/logs", to: "logs#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
