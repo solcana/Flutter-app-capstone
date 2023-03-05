eb7ea9caf588265660ceafeefeae10b841209391
Ana Solcan <solcan.anna@gmail.com>
2023-03-05 19:38:14 +0200

    added a my_logs page that only display specific logs to a specific user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ resources :travel_logs
 resources :users
 
   get "/logs", to: "logs#index"
+  get 'my_logs', to: 'logs#my_logs'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
