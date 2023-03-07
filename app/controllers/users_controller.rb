9043c01867e75a870d9c94cb13b7fc72c227e85b
Ana Solcan <solcan.anna@gmail.com>
2023-03-07 14:02:38 +0200

    able to sign up new user, display on navbar: current users email, added destroy method to user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+    def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to new_user_registration_path, notice: "User deleted."
+  end
+end
