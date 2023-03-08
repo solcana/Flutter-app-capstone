d7af89040a068f0e953ba49e38bdfc62f6148fe4
Ana Solcan <solcan.anna@gmail.com>
2023-03-08 20:06:44 +0200

    working app, with a small bug related to routing, and comments model not done yet

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,20 @@ class CommentsController < ApplicationController
     redirect_to travel_log_path(@travel_log)
   end
 
+  def destroy
+   @travel_log = TravelLog.find(params[:travel_log_id])
+   @comment = @travel_log.comments.find(params[:id])
+   @comment.destroy
+  redirect_to travel_log_path(@travel_log)
+  end
+
+  def update
+    @travel_log = TravelLog.find(params[:travel_log_id])
+    @comment = @travel_log.comments.find(params[:id])
+    @comment.update(comment_params)
+      redirect_to travel_log_path(@travel_log)
+    end
+
   private
     def comment_params
      params.require(:comment).permit(:content, :user_id)
