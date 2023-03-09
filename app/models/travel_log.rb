8eb5328cd08c73e38394d905103ddac7149432dd
Ana Solcan <solcan.anna@gmail.com>
2023-03-09 14:34:31 +0200

    adjusted a bit the styling for comments section

--- app/models/travel_log.rb
+++ app/models/travel_log.rb
@@ -2,9 +2,8 @@ class TravelLog < ApplicationRecord
   belongs_to :user
   belongs_to :destination
 
-  has_many :comments, dependent: :destroy
+  has_many :comments
 
   validates :destination_id, :title, presence: true
 
 end
-
